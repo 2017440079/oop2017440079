c507644d1adc9a050f256e7194ae6fa325cfdb5a
2017440079 <37829839+2017440079@users.noreply.github.com>
2018-06-05 17:48:34 +0900

    Add files via upload

---
+++ 20180605/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Animal aDog = new Dog("Tina");//upcasting 
+		Animal aCat = new Cat("¸Á°í");//upcasting
+		aDog.sing();
+		//error aDog.playFetch();
+		((Dog)aDog).playFetch();//downcasting
+		//error (Cat)aDog).playFetch();
+		((Dog)aCat).playFetch();
+		aCat.sing();
+	}
+
+}
